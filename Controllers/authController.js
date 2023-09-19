041d07c0edbe3fab71728efc3617167315d34702
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-09-19 08:50:59 +0300

    first

---
+++ Controllers/authController.js
@@ -0,0 +1,58 @@
+const User = require("../Models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const secretKey = "your-secret-key";
+
+const signup = async (req, res) => {
+  try {
+    const { username, email, full_name, password } = req.body;
+
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username or email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      full_name,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
+
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error("Signup failed:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
+
+    res.json({ token });
+  } catch (error) {
+    console.error("Login failed:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const authRoutes = require("./Routes/authRoutes");
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const dbURL = "mongodb://localhost:27017/parametric-superform-generator";
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(cors());
+app.use("/auth", authRoutes);
