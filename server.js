cb5d79595efa37fd3e5000645d4ddb6eba7096d9
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-11-01 22:41:52 +0300

    Code cleanup

--- server.js
+++ server.js
@@ -3,24 +3,18 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const routes = require("./src/routes/routes")
 const cors = require("cors")
+require("./src/database/db")
 require("dotenv").config()
 
 const app = express()
 const port = process.env.NODE_ENV === "production" ? process.env.PORT : 3000
-const dbURL = process.env.MONGO_URL || "mongodb://localhost:27017/mern-auth"
+// const dbURL = process.env.MONGO_URL || "mongodb://localhost:27017/mern-auth"
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 app.use("/api", routes)
 
-// NOTE: MongoDB connection
-mongoose
-  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB")
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`)
-    })
-  })
-  .catch((err) => console.error("MongoDB connection error:", err))
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
