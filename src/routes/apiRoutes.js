a973c35c0b323ada61b79eec4bc9fc9fcf8334cd
TiavinaCyri <tiavinacyri@gmail.com>
2023-11-20 21:17:09 +0300

    fix bug

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-const dashboardController = require("../controllers/dashboardController")
 const reportController = require("../controllers/reportController")
 const userController = require("../controllers/userController")
-const { register, login } = require("../controllers/authenticationController")
 
 // Routes pour les rapports
 router.post("/reports", reportController.createReport)
@@ -18,16 +16,5 @@ router.get("/users", userController.getAllUsers)
 router.get("/users/:userId", userController.getUser)
 router.put("/users/:userId", userController.updateUser)
 router.delete("/users/:userId", userController.deleteUser)
-
-// Route pour l'inscription
-router.post("/register", register)
-
-// Route pour la connexion
-router.post("/login", login)
-
-// Route pour mettre à jour le profil de l'administrateur
-router.put("/profile", dashboardController.updateAdminProfile)
-// Route pour obtenir des statistiques du tableau de bord
-router.get("/stats", dashboardController.getDashboardStats)
 
 module.exports = router
