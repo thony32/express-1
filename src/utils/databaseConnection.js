0b1d476828391bac9539a043bcd78ffbd7ae7705
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-11-20 23:06:37 +0300

    database connection

--- src/utils/databaseConnection.js
+++ src/utils/databaseConnection.js
@@ -1,14 +1,15 @@
-'use strict';
-const mysql = require('mysql2');
+require("dotenv").config()
+"use strict"
+const mysql = require("mysql2")
 //local mysql db connection
 const databaseConnection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'proref-db'
-});
-databaseConnection.connect(function(err) {
-  if (err) throw err;
-  console.log("Database Connected!");
-});
-module.exports = databaseConnection;
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+})
+databaseConnection.connect(function (err) {
+  if (err) throw err
+  console.log("Database Connected!")
+})
+module.exports = databaseConnection
